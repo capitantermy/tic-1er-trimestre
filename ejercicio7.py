63ca1a99e51fa34a32628d45edb6535d156022e7
josé <55531215+capitantermy@users.noreply.github.com>
2019-11-21 09:28:45 +0100

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,19 @@
+def ejercicio7():
+    print 's=suma r=resta m=multiplicación d=división'
+    a=input("primer numero entero ")
+    b=input("segundo numero entero ")
+    c=raw_input("escribe una letra ")
+    if c=="s":
+        print a+b
+    if c=="r":
+        print a-b
+    if c=="m":
+        print a*b
+    if c=="d":
+            if b==0:
+                j=input("otro numero")
+                3print a/j
+            else:
+                    print a/b
+        
+ejercicio7()
